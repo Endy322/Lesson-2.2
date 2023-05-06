8b9941f3cf092bae7df6f1d81d76d443aaef39e0
Endy322 <pwsinendy@yandex.ru>
2023-05-06 10:49:28 +0300

    Outhlets

--- Lesson 2.2/ViewController.swift
+++ Lesson 2.2/ViewController.swift
@@ -9,9 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var trafficLightBody: UIView!
+    @IBOutlet var startTrafficLight: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startTrafficLight.layer.cornerRadius = 15
     }
 
 
