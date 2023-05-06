f9bf1da2dc3645844684892de716479d0ecb8d79
Endy322 <pwsinendy@yandex.ru>
2023-05-06 18:23:17 +0400

    code

--- Lesson 2.2/ViewController.swift
+++ Lesson 2.2/ViewController.swift
@@ -8,19 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet var trafficLightBody: UIView!
-    @IBOutlet var startTrafficLight: UIButton!
     
+    @IBOutlet var trafficLightBody: UIView!
     @IBOutlet var redWindow: UIView!
     @IBOutlet var yellowWindow: UIView!
     @IBOutlet var greenWindow: UIView!
     
+    @IBOutlet var startTrafficLight: UIButton!
+    
+    private let trafficLightOff: CGFloat = 0.3
+    private let trafficLightOn: CGFloat = 1
+    private var currentLight = CurrentLight.red
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        redWindow.alpha = 0.3
-        yellowWindow.alpha = 0.3
-        greenWindow.alpha = 0.3
+        
+        redWindow.alpha = trafficLightOn
+        yellowWindow.alpha = trafficLightOff
+        greenWindow.alpha = trafficLightOff
         
         trafficLightBody.layer.cornerRadius = 15
         startTrafficLight.layer.cornerRadius = 15
@@ -30,10 +35,27 @@ class ViewController: UIViewController {
         greenWindow.layer.cornerRadius = 90
         
     }
-
+    
+    extension ViewController {
+            enum CurrentLight {
+            case red, yellow, green
+        }
+    }
+    
+    
+    
     @IBAction func switchingTrafficLight(_ sender: UIButton) {
-       
+        if startTrafficLight.currentTitle == "Start" {
+            startTrafficLight.setTitle("Next", for: .normal)
+            
+            switch currentLight {
+                
+            }
+            
+        }
+        
     }
     
 }
+
 
